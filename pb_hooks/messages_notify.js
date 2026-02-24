5866fcad3102c9a9633b53ae4ef8634cb478d4c3
Carlos Perales Ayala <carlosperalesa@gmail.com>
2026-02-23 21:24:04 -0300

    fix: actualizar notificación de mensajes

--- pb_hooks/messages_notify.js
+++ pb_hooks/messages_notify.js
@@ -8,7 +8,9 @@ onRecordAfterCreate("messages", async (e) => {
       <h2>Nuevo mensaje</h2>
       <p><strong>Nombre:</strong> ${msg.name}</p>
       <p><strong>Email:</strong> ${msg.email}</p>
-      <p><strong>Mensaje:</strong><br>${msg.content}</p>
+      <p><strong>Teléfono:</strong> ${msg.phone}</p>
+      <p><strong>Asunto:</strong> ${msg.subject}</p>
+      <p><strong>Mensaje:</strong><br>${msg.message}</p>
     `
     });
 });
